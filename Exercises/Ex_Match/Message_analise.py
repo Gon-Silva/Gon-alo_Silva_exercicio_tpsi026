cb9ebcaf8b633bf48a36e74d0940dee0143ff6ab
Gon-Silva <goncalo.silva.t0123533@edu.atec.pt>
2026-03-09 20:50:56 +0000

    Criei uma nova pasta\Diretório foca para exercícios

---
+++ Exercises/Ex_Match/Message_analise.py
@@ -0,0 +1,14 @@
+# Function that check the input of the user
+
+
+# Main fuction
+def main():
+    print("Print ")
+
+    user_input = input("\nEnter one type of data: ")
+    check_type_data(user_input)
+
+
+# Start the program
+if __name__ == "__main__":
+    main()
